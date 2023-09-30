aaf488f2b7579d781ae21eedd9745adf16a1f82a
HarinKhakhi <k.harin2306@gmail.com>
2023-09-30 02:23:31 -0700

    Submission

--- src/breadth_first.cpp
+++ src/breadth_first.cpp
@@ -1,8 +1,39 @@
 #include "tree_algorithms.h"
 
-// TODO
 void _breadth_first(TreeNode* root, vector<int>& answer){
+    if(root == nullptr) return;
     
+    // current level
+    vector<TreeNode*> level;
+    level.push_back(root);
+
+    // iterate until we have exhausted all nodes
+    while(level.size()!=0){
+        // to store next level nodes that will be
+        // traversed in next iteration
+        vector<TreeNode*> newLevel;
+
+        // for each node in current level, add all its 
+        // non-null child to nextLevel
+        for(TreeNode* node: level){
+
+            // add current node data to answer
+            answer.push_back(node -> data);
+
+            // add if non-null
+            if(node->left)
+            newLevel.push_back(node->left);
+
+            // add if non-null
+            if(node->right)
+            newLevel.push_back(node->right);
+        }
+
+        // building next level
+        level.clear();
+        for(TreeNode* node: newLevel)
+        level.push_back(node);
+    }
 }
 
 vector<int> breadth_first(TreeNode* node){
--- src/depth_first.cpp
+++ src/depth_first.cpp
@@ -8,15 +8,20 @@ void _inorder(TreeNode* node, vector<int>& answer){
     _inorder(node->right, answer);
 }
 
-
-// TODO
 void _preorder(TreeNode* node, vector<int>& answer){
-    
+    if(node == nullptr) return;
+
+    answer.push_back(node->data);
+    _preorder(node->left, answer);
+    _preorder(node->right, answer);
 }
 
-// TODO
 void _postorder(TreeNode* node, vector<int>& answer){
-    
+    if(node == nullptr) return;
+
+    _postorder(node->left, answer);
+    _postorder(node->right, answer);
+    answer.push_back(node->data);
 }
 
 vector<int> inorder(TreeNode* node){
